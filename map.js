afafd7fffd61661e0af376c7c25b6b0596e3c6d9
Luca Simone <info@lucasimone.info>
2013-09-23 11:08:14 +0200

    Cleaning

--- map.js
+++ map.js
@@ -10,15 +10,15 @@ var app = angular.module('mapApp', []);
 app.directive('map', function() {
   return {
     restrict: 'E',
-    priority: 0,
     scope: true,
     controller: function($scope) {
     
       this.map = {};
  
       this.getMap = function() {
-        return this.map.map;
+        return this.map;
       }
+      
     },
     link: function($scope, element, attrs, controller) {
       controller.map = new esri.Map(element[0]);
@@ -29,13 +29,12 @@ app.directive('map', function() {
 app.directive('layer', function() {
   return {
     restrict: 'E',
-    priority: 1,
     require: '^map',
     scope: false,
     link: function($scope, element, attrs, mapCtrl) {
     
       $scope.$watch(mapCtrl.map, function() {
-        mapCtrl.map.addLayer(new esri.layers.ArcGISTiledMapServiceLayer(attrs.url));
+        mapCtrl.getMap().addLayer(new esri.layers.ArcGISTiledMapServiceLayer(attrs.url));
       })
       
     }
