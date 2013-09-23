392ad380c7805ca2ec5fac761e80b30e21747d68
Luca Simone <info@lucasimone.info>
2013-09-23 11:04:46 +0200

    It works!

--- map.js
+++ map.js
@@ -1,35 +1,47 @@
-dojo.require("esri.map");
+﻿dojo.require("esri.map");
 dojo.addOnLoad(init);
 
 function init() {
-	angular.bootstrap(document, ['mapApp']);
+  angular.bootstrap(document, ['mapApp']);
 }
 
 var app = angular.module('mapApp', []);
 
 app.directive('map', function() {
-	return {
-		restrict: 'E',
-		controller: function($scope) {
-			this.getMap = function() {
-				return $scope.map;
-			}
-		},
-		link: function($scope, element, attrs) {
-			$scope.map = new esri.Map(element[0]);
-		}
-	}
+  return {
+    restrict: 'E',
+    priority: 0,
+    scope: true,
+    controller: function($scope) {
+    
+      this.map = {};
+ 
+      this.getMap = function() {
+        return this.map.map;
+      }
+    },
+    link: function($scope, element, attrs, controller) {
+      controller.map = new esri.Map(element[0]);
+    }
+  }
 });
 
 app.directive('layer', function() {
-	return {
-		restrict: 'E',
-		require: '^map',
-		link: function(scope, element, attrs, MapCtrl) {
-			MapCtrl.getMap().addLayer(new esri.layers.ArcGISDynamicMapServiceLayer(attrs.url));
-		}
-	}
+  return {
+    restrict: 'E',
+    priority: 1,
+    require: '^map',
+    scope: false,
+    link: function($scope, element, attrs, mapCtrl) {
+    
+      $scope.$watch(mapCtrl.map, function() {
+        mapCtrl.map.addLayer(new esri.layers.ArcGISTiledMapServiceLayer(attrs.url));
+      })
+      
+    }
+  }
 });
 
-app.controller('FirstMapCtrl', ['$scope', function($scope) {	
+app.controller('FirstMapCtrl', ['$scope', function($scope) {
+
 }]);
